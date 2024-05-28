64ffed8cf03c0f81f8d1cd91de600abfc4ddd2b7
AntoineRai <antoine.raimand@gmail.com>
2024-05-28 17:02:05 +0200

    List Of Candidate et Employee

--- src/main/java/com/example/fullstackinterro/controller/CandidateController.java
+++ src/main/java/com/example/fullstackinterro/controller/CandidateController.java
@@ -1,9 +1,14 @@
 package com.example.fullstackinterro.controller;
 
+import com.example.fullstackinterro.model.Candidate;
 import com.example.fullstackinterro.service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/candidates")
@@ -11,4 +16,11 @@ public class CandidateController {
 
     @Autowired
     private CandidateService candidateService;
+
+    @GetMapping
+    private String allCandidates(Model model){
+        List<Candidate> candidates = candidateService.findAll();
+        model.addAttribute("candidates", candidates);
+        return "candidate/list";
+    }
 }
--- src/main/java/com/example/fullstackinterro/controller/EmployeeController.java
+++ src/main/java/com/example/fullstackinterro/controller/EmployeeController.java
@@ -1,12 +1,17 @@
 package com.example.fullstackinterro.controller;
 
+import com.example.fullstackinterro.model.Employee;
 import com.example.fullstackinterro.model.Vacation;
 import com.example.fullstackinterro.service.AbsenceService;
 import com.example.fullstackinterro.service.EmployeeService;
 import com.example.fullstackinterro.service.VacationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
@@ -20,4 +25,11 @@ public class EmployeeController {
 
     @Autowired
     private VacationService vacationService;
+
+    @GetMapping
+    public String allEmployees(Model model){
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees", employees);
+        return "employee/list";
+    }
 }
